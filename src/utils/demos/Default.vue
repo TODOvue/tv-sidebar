08c0050b76cfb254499521e6bdfef17cf390ae79
Cristhian Daza <cristian.d.b@hotmail.com>
2025-10-19 21:33:00 -0500

    feat(TvSidebar): add initial styles, variables, and sidebar components

---
+++ src/utils/demos/Default.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- The link tag is sent to be used in the blog with nuxt -->
+  <tv-sidebar linkTag="nuxt-link" :data="list" />
+</template>
+
+<script>
+import { TvSidebar } from '@todovue/tv-sidebar';
+
+const list = {
+  title: "Most Popular Blogs",
+  list: [
+    {
+      id: 1,
+      title: "10 Tips for Creating a Successful YouTube Channel",
+      link: "/10-tips-for-creating-a-successful-youtube-channel",
+    },
+    // ...
+  ],
+};
+</script>
---
+++ src/utils/demos/imageClickable.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- The link tag is sent to be used in the blog with nuxt -->
+  <tv-sidebar isImage clickable :data="image" />
+</template>
+
+<script>
+import { TvSidebar } from '@todovue/tv-sidebar';
+
+const image = {
+  title: "TODOvue Blog",
+  image: {
+    src: "https://todovue.com/vue.webp",
+    alt: "Image",
+    link: "https://todovue.com/",
+  }
+};
+</script>
